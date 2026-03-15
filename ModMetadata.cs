2db22a83e81665d19a76aded30a4cd7a50bb1e39
Szonszczyk <Szonszczyk@users.noreply.github.com>
2026-03-15 15:52:49 +0100

    Updated version

--- ModMetadata.cs
+++ ModMetadata.cs
@@ -8,7 +8,7 @@ namespace DefinitiveWeaponVariants
         public override string Name { get; init; } = "DefinitiveWeaponVariants";
         public override string Author { get; init; } = "Szonszczyk";
         public override List<string>? Contributors { get; init; }
-        public override SemanticVersioning.Version Version { get; init; } = new("4.1.1");
+        public override SemanticVersioning.Version Version { get; init; } = new("4.1.2");
         public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.4");
         public override List<string>? Incompatibilities { get; init; } = [];
         public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = [];
